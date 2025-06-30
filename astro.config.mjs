3a602ffb2a08bc8f28e81f27fbd4ebea4f578349
Vivanco, Diego <Diego.Vivanco@wolterskluwer.com>
2025-06-30 12:48:29 +0200

    chore: update package.json for deployment scripts and add wrangler configuration
    - Added deploy and deploy:preview scripts to package.json for easier deployment to Cloudflare Pages.
    - Included @astrojs/cloudflare as a dependency for Cloudflare integration.
    - Introduced wrangler as a devDependency for managing Cloudflare Workers.
    - Created a new wrangler.toml file with project configuration for compatibility and build command.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 import vue from '@astrojs/vue';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'hybrid',
+  adapter: cloudflare(),
   integrations: [vue()]
 });
